9f9d0e0fe064eef663205b6ee479f131ea71be0f
木村 光輝 <67196597+telzo2000@users.noreply.github.com>
2023-06-26 05:05:53 +0900

    fix

--- firmware/keymaps/default/keymap.c
+++ firmware/keymaps/default/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Z,   KC_X,   KC_C,   KC_V,  KC_B,  KC_N,  KC_M,  KC_COMMA,KC_DOT,KC_LSFT,
         KC_LCTRL,       KC_RGUI, LT(1,KC_SPACE),LT(2,KC_SPACE),KC_LALT,KC_BSPACE
     ),
-    [4] = LAYOUT_ortho_4x4(
+    [3] = LAYOUT_ortho_4x4(
         KC_1,   KC_2,   KC_3,   KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,
         KC_A,   KC_S,   KC_D,   KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_ENT,
         KC_Z,   KC_X,   KC_C,   KC_V,  KC_B,  KC_N,  KC_M,  KC_COMMA,KC_DOT,KC_LSFT,
